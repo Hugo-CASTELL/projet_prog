dfa1165e2d7f34b4b03bcab9769d15f35fb39164
Hugo-CASTELL <hugo.castell@outlook.fr>
2025-01-08 15:30:16 +0100

    Updated binary tree tests

--- src/tests/tests_binary_tree.adb
+++ src/tests/tests_binary_tree.adb
@@ -12,6 +12,39 @@ procedure Tests_Binary_Tree is
 
 begin
   Init (Tree, 'f');
+
+  -- Testing initialisation
+  pragma Assert(GetData(Tree) = 'p');
+
+  -- Testing if branches are empty
+  pragma Assert(IsBranchEmpty(Tree, True) = True);
+  pragma Assert(IsBranchEmpty(Tree, False) = True);
+
+  -- Testing size
+  pragma Assert(GetSize(Tree) = 0);
+
+  -- Set values to branches
   SetLeft (Tree, 'l');
   SetRight (Tree, 'r');
+
+  -- Testing assignation of branches
+  pragma Assert(GetData(GetLeft(Tree)) = 'l');
+  pragma Assert(GetData(GetRight(Tree)) = 'r');
+
+  -- Testing size with (and of) branches
+  pragma Assert(GetSize(Tree) = 2);
+  pragma Assert(GetSize(GetLeft(Tree)) = 0);
+  pragma Assert(GetSize(GetRight(Tree)) = 0);
+
+  -- Testing leaf detection
+  pragma Assert(IsLeaf(GetLeft(Tree)) = True);
+  pragma Assert(IsLeaf(GetRight(Tree)) = True);
+
+  -- Clearing the right branch
+  ClearRight(Tree);
+
+  -- Testing clearing
+  pragma Assert(IsBranchEmpty(Tree, False) = True);
+  pragma Assert(GetSize(Tree) = 1);
+
 end Tests_Binary_Tree;
