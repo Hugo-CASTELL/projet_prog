6e48025fb3440016194d42c916d53dc7b86728e3
n7student <arthlevi@gmail.com>
2025-01-20 10:58:02 +0100

    Reecriture de liste.adb

--- src/liste.adb
+++ src/liste.adb
@@ -1,78 +1,78 @@
-package body Liste is
+package body List is
 
-   -- Initialiser une liste
-   procedure Initialiser(Liste : out T_Liste) is
+   -- Initialize a list
+   procedure Init(List : out T_List) is
    begin
-      Liste.Taille := 0;
-   end Initialiser;
+      List.Taille := 0;
+   end Init;
 
-   -- Ajouter un élément à la liste
-   procedure Ajouter(Liste : in out T_Liste; Element : in T_Type) is
+   -- Add an element to the list
+   procedure Add(List : in out T_List; Element : in T_Type) is
    begin
-        Liste.Taille := Liste.Taille + 1;
-        Liste.Contenu(Liste.Taille) := Element;
-   end Ajouter;
+        List.Taille := List.Taille + 1;
+        List.Contenu(List.Taille) := Element;
+   end Add;
 
-   -- Supprimer le dernier élément de la liste
-   procedure Supprimer(Liste : in out T_Liste; Index : in Natural) is
+   -- Delete the last element of the list
+   procedure Delete(List : in out T_List; Index : in Natural) is
    begin
-        for I in Index..(Liste.Taille-1) loop
-            Liste.Contenu(I) := Liste.Contenu(I+1);
+        for I in Index..(List.Taille-1) loop
+            List.Contenu(I) := List.Contenu(I+1);
         end loop;
-        Liste.Taille := Liste.Taille - 1;
-   end Supprimer;
+        List.Taille := List.Taille - 1;
+   end Delete;
 
-    -- Modifie un élément à la liste
-   procedure Modifier(Liste : in out T_Liste; Element : in T_Type; Index : in Natural) is
+    -- Modify an element of the list
+   procedure Modify(List : in out T_List; Element : in T_Type; Index : in Natural) is
    begin
-        Liste.Contenu(Index) := Element;
-   end Modifier;
+        List.Contenu(Index) := Element;
+   end Modify;
 
-   -- Insere un élément à la liste
-   procedure Inserer(Liste : in out T_Liste; Element : in T_Type; Index : in Natural) is
+   -- Insert an element to the list
+   procedure Insert(List : in out T_List; Element : in T_Type; Index : in Natural) is
    begin
-        Liste.Taille := Liste.Taille + 1;
-        for I in reverse (Index+1)..Liste.Taille loop
-            Liste.Contenu(I) := Liste.Contenu(I-1);
+        List.Taille := List.Taille + 1;
+        for I in reverse (Index+1)..List.Taille loop
+            List.Contenu(I) := List.Contenu(I-1);
         end loop;
-        Liste.Contenu(Index) := Element;
-   end Inserer;
+        List.Contenu(Index) := Element;
+   end Insert;
 
-   -- Concatene 2 listes
-   procedure Concatener(Liste1 : in out T_Liste; Liste2 : in T_Liste) is
-     Taille_Liste1 : Natural;
-     Taille_Liste2 : Natural;
+   -- Add list 2 to list 1
+   procedure Concat(List1 : in out T_List; List2 : in T_List) is
+     Taille_List1 : Natural;
+     Taille_List2 : Natural;
    begin
-        Taille_Liste1 := Taille(Liste1);
-        Taille_Liste2 := Taille(Liste2);
-        Liste1.Taille := Taille_Liste1 + Taille_Liste2;
-        for I in 1..Taille_Liste2 loop
-            Liste1.Contenu(Taille_Liste1+I) := Liste2.Contenu(I);
+        Taille_List1 := Taille(List1);
+        Taille_List2 := Taille(List2);
+        List1.Taille := Taille_List1 + Taille_List2;
+        for I in 1..Taille_List2 loop
+            List1.Contenu(Taille_List1+I) := List2.Contenu(I);
         end loop;
-   end Concatener;
+   end Concat;
 
-   -- Obtenir un élément de la liste par index
-   function Obtenir(Liste : T_Liste; Index : in Natural) return T_Type is
+   -- Get an element from the list given its index
+   function Get(List : in T_List; Index : in Natural) return T_Type is
    begin
-        return Liste.Contenu(Index);
-   end Obtenir;
+        return List.Contenu(Index);
+   end Get;
 
-   -- Vérifier si la liste est pleine
-   function Est_Pleine(Liste : in T_Liste) return Boolean is
+   -- Check if the list is full
+   function IsFull(List : in T_List) return Boolean is
    begin
-      return Liste.Taille = Capacite;
-   end Est_Pleine;
+      return List.Taille = Capacity;
+   end IsFull;
 
-   -- Vérifier si la liste est vide
-   function Est_Vide(Liste : in T_Liste) return Boolean is
+   -- Check if the list is empty
+   function IsEmpty(List : in T_List) return Boolean is
    begin
-      return Liste.Taille = 0;
-   end Est_Vide;
+      return List.Taille = 0;
+   end IsEmpty;
 
-   -- Renvoie le nombre d'élements de la liste
-   function Taille(Liste : in T_Liste) return Natural is
+   -- Determinate the size of the list
+   function GetSize(List : in T_List) return Natural is
    begin
-          return Liste.Taille;
-   end Taille;
+          return List.Taille;
+   end GetSize;
 
-end Liste;
+end List;
